193637dc59cd95cf99b692fa0bb794f4731a87dd
wj <wongjuanaa@gmail.com>
2025-08-21 20:16:00 -0400

    init repo with mvp of CareerCheer!

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
